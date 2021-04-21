cdb771b08053b915be0e962ccd3468af5011fb55
Benjamin Democh <benjamin.democh@student.hamk.fi>
2021-04-21 10:03:16 +0300

    added lable for date and time

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 import tkinter.messagebox as tkBox
 import tkinter.font as tkFont
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
 
-#%% Class application
+
+# %% Class application
 class App:
 
     def __init__(self, root):
@@ -21,15 +20,27 @@ class App:
                                     (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
-# %% Lable        
-        self.__Lable_01 = tk.Label(root)
+# %% Lable date
+        t = time.localtime()
+        current_date = time.strftime("%A %d %B %Y", t)
+        current_time = time.strftime("%H:%M", t)
+        self.__TimeandWeather = tk.Label(root)
         ft = tkFont.Font(family='Times', size=11)
-        self.__Lable_01["font"] = ft
-        self.__Lable_01["fg"] = "#333333"
-        self.__Lable_01["justify"] = "right"
-        self.__Lable_01["text"] = "present time and weather"
-        self.__Lable_01.place(x=350, y=50, width=170, height=25)
-        
+        self.__TimeandWeather["font"] = ft
+        self.__TimeandWeather["fg"] = "#333333"
+        self.__TimeandWeather["justify"] = "right"
+        self.__TimeandWeather["text"] = current_date
+        self.__TimeandWeather.place(x=350, y=50, width=170, height=25)
+
+        self.__TimeandWeather = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=11)
+        self.__TimeandWeather["font"] = ft
+        self.__TimeandWeather["fg"] = "#333333"
+        self.__TimeandWeather["justify"] = "right"
+        self.__TimeandWeather["text"] = current_time
+        self.__TimeandWeather.place(x=290, y=70, width=170, height=25)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
